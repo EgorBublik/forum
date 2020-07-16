7e84a3c809b53d8dfbf8f73386efb53f1686e657
EgorBublik <egorka.bublik@mail.ru>
2020-07-16 15:51:25 +0300

    first commit

--- src/components/new-question/new-question.js
+++ src/components/new-question/new-question.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import './new-question.css';
+import axios from 'axios';
 
-const NewQuestion = () => {
-    return (
-        <div className="new-question container">
-            <div className="col-12">
-                <textarea className='new-question-input' placeholder='Задай свой вопрос'/>
+export default class newQuestion extends React.Component {
+    state = {
+        questions: '',
+    }
+
+    handleChange = event => {
+        this.setState({ questions: event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const question = {
+            questions: this.state.questions
+        };
+
+        axios.post(`http://localhost:3005/questions`, { question })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
+    render() {
+        return (
+            <div className="new-question container">
+                <div className="col-12">
+                    <textarea className='new-question-input' placeholder='Задай свой вопрос' onChange={this.handleChange}/>
+                </div>
+                <div className="col-12">
+                    <button type="button" className="btn btn-primary">
+                        Задать вопрос
+                    </button>
+                </div>
             </div>
-            <div className="col-12">
-                <button type="button" className="btn btn-primary">
-                    Задать вопрос
-                </button>
-            </div>
-        </div>
-    )
+        )
+    }
+
 }
 
-export default NewQuestion;
+// export default NewQuestion;
