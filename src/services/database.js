9f8eb76fc40fb88f35e44e6c5a6f5f3af50a0e84
EgorBublik <egorka.bublik@mail.ru>
2020-10-08 12:13:33 +0300

    update gitignore

--- src/services/database.js
+++ src/services/database.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const getAll = async () => {
-    const res = await axios.get(process.env.REACT_APP_URL_BACK);
+    const res = await axios.get(process.env.REACT_APP_URL_BACK+'questions');
     return res.data;
 };
 
 export const postAll = async (info) => {
-    const res = await axios.post(process.env.REACT_APP_URL_BACK, info);
+    const res = await axios.post(process.env.REACT_APP_URL_BACK+'questions', info);
     console.log(info);
     return res.data;
 }
